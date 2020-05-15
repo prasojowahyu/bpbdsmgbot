b69e3a7c107551f8ada770085c15217c94313b32
Wahyu Prasojo <wahyuprasojo@students.unnes.ac.id>
2020-05-15 10:46:57 +0700

    Testing first bot, connecting and simple command :)

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-	echo "halo hooman!";
+	require_once 'vendor/autoload.php';
+	$bot = new PHPTelebot('1167185320:AAEpswmETV0XF1egPH24dCKrCwnnWaFDRlw', '@bpbdsemarang_6php1bot'); // Bot username is optional, its required for handle command that contain username (/command@username) like on a group.
+	
+	// Simple command
+	$bot->cmd('/start', 'Hello there :)');
+	$bot->cmd('/ping', 'PONG!!');
+
+	//run run run!!
+	$bot->run();
 
 ?>
