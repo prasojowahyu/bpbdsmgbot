c26981aa5e2ea6648387f615b86278850ba73053
Wahyu Prasojo <wahyuprasojo@students.unnes.ac.id>
2020-05-17 16:25:46 +0700

    Syntax moved; syntax sub menu button pindah ke menuButton.php

---
+++ menuButton.php
@@ -0,0 +1,23 @@
+<?php 
+    //berisi syntax untuk submenu button di index
+    
+    #Kabar Terbaru
+	//isi dari menu button Kabar
+	$bot->cmd('Kabar Terbaru', function() {
+		//inisialisasi
+		$text		= "Kabar Terbaru Untuk: \n";
+
+		//menu button
+		$keyboard[]	= ['Semua Kabar', 'Bencana', 'Cuaca'];
+
+		//telebot plugin
+		$option		= [
+			'reply_markup' => ['keyboard'=> $keyboard,
+			'resize_keyboard'=> true
+			]
+		];
+
+		return Bot::sendMessage($text, $option);
+	});
+
+?>
