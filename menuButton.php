c9ee0310a2d3270a7a129b9dd06e0f6663f64a3f
Wahyu Prasojo <wahyuprasojo@students.unnes.ac.id>
2020-05-31 14:27:54 +0700

    hapus tombol semua di menu button

--- menuButton.php
+++ menuButton.php
@@ -9,7 +9,7 @@
 		$text		= "Kabar Terbaru Untuk: \n";
 
 		//menu button
-		$keyboard[]	= ['Semua', 'Bencana', 'Cuaca'];
+		$keyboard[]	= ['Bencana', 'Cuaca'];
 		$keyboard[] = ['Home'];
 
 		//telebot plugin
@@ -21,17 +21,6 @@
 
 		return Bot::sendMessage( $text, $option );
 	});
-		#Item-Menu: Semua
-		$bot->cmd('Semua', function() {
-			//add caption
-			$option		= [
-				'caption'	=> 'THISISCAPTION'
-			];
-			//ambil id file gambar dari json file yg dikirim user lewat tele
-			$photo_id	= 'AgACAgUAAxkBAAMrXsHg7ltBB81Y05hpUUIVXxWpFb0AAsqpMRufVRBWor5uB0lOAdM5bxprdAADAQADAgADbQADkJoAAhkE';
-			
-			return Bot::sendPhoto( $photo_id, $option );
-		});
 		#Item-Menu: Bencana
 		$bot->cmd('Bencana', function() {
 			
