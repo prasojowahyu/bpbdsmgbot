b50e8530f07ac3766bf62cb1edfaaf0028da00e2
Wahyu Prasojo <wahyuprasojo@students.unnes.ac.id>
2020-06-03 22:30:15 +0700

    tambah syntax item menu bencana, versi sebelumnya terhapus

--- menuButton.php
+++ menuButton.php
@@ -21,6 +21,12 @@
 		return Bot::sendMessage( $text, $option );
 	});
 		#Item-Menu: Bencana
+		$bot->cmd('Bencana', function() {
+			$url	= "Link menuju berita bencana terbaru \n http://bpbd.semarangkota.go.id/category/berita";
+			return Bot::sendMessage( $url );
+		});
+		
+		#Item-Menu: Cuaca
 		$bot->cmd('Cuaca', function() {
 			$link	= "https://www.bmkg.go.id/cuaca/prakiraan-cuaca.bmkg?Kota=Semarang&AreaID=501262&Prov=35";
 			$url	= "Informasi Cuaca terbaru di Kota Semarang, \nSumber: bmkg \n" .$link;
