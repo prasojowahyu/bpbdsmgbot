290596376b7a46e7eda390292e4f01896a7b3684
Wahyu Prasojo <wahyuprasojo@students.unnes.ac.id>
2020-06-15 13:15:16 +0700

    icon

--- menuButton.php
+++ menuButton.php
@@ -73,7 +73,7 @@
 	$bot->cmd('Contact', function() {
 		
 		$keyboard[] = [
-			['text' => '🌐 WhatsApp BPBD Semarang', 'url' => 'https://api.whatsapp.com/send?phone=628122010051'],
+			['text' => '📢 WhatsApp BPBD Semarang', 'url' => 'https://api.whatsapp.com/send?phone=628122010051'],
 		];
 		$option		= [
 			'reply_markup'	=> ['inline_keyboard' => $keyboard],
