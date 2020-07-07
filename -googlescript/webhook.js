573af5d24d6920fb6baefb9a2b70dd25f20a38d0
Wahyu Prasojo <wahyuprasojo@students.unnes.ac.id>
2020-07-07 20:18:09 +0700

    delete folder

--- -googlescript/webhook.js
+++
@@ -1,9 +0,0 @@
-// Isi dengan web App URL yang di dapat saat deploy
-var webAppUrl = "https://script.google.com/macros/s/AKfycbxNc1jpEVJGWNCePsfw7-fEljQvmDMso31mXN5mCm9LAGfLq1o/exec";
-
-function setWebHook() {
-    var result = tg.request('setWebhook', {
-        url: webAppUrl
-    });
-    Logger.log(result);
-}
